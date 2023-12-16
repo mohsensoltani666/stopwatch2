9d702399f983a7dc95f973327734aad5b707180d
Mohsen Soltani <mohsensoltani@Mohsens-MacBook-Pro.local>
2023-12-16 13:43:37 +0330

    modifi in css

--- src/components/stopwatch/stopwatch.js
+++ src/components/stopwatch/stopwatch.js
@@ -1,22 +1,20 @@
-
-import { useState } from 'react';
-import ButtonTimer from '../buttonTimer/buttonTimer';
-import DisplayTimer from '../displayTimer/displayTimer';
-import './stopwach.css';
-import Save from '../displaySave/displaSave';
+import { useState } from "react";
+import ButtonTimer from "../buttonTimer/buttonTimer";
+import DisplayTimer from "../displayTimer/displayTimer";
+import "./stopwach.css";
+import Save from "../displaySave/displaSave";
 
 function Stopwatch() {
-
-  const [time , setTime] = useState({ms:0 , s:0 , m:0 , h:0 });
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
 
   return (
     <div className="continer-stopwatch">
-      <div className='top'>
-        <h1 className='h1-stopwatch'>stopwatchii</h1>
+      <div className="top">
+        <h1 className="h1-stopwatch">stopwatch</h1>
         <DisplayTimer time1={time} />
       </div>
-      <Save  display={setTime} />
-      <ButtonTimer time2={time} setTime1={setTime}/>
+      <Save display={setTime} />
+      <ButtonTimer time2={time} setTime1={setTime} />
     </div>
   );
 }
